a0e5177fc8fdfbcf674008dad86979978fce953a
aliff <aliff.rosli96@gmail.com>
2022-01-31 12:50:19 +0800

    add relation orm to workspace and task

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Task;
+use App\Models\Workspace;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -41,4 +43,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function workspace()
+    {
+        return $this->hasMany(Workspace::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
