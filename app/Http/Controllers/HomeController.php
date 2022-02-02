566e3e24ac3be564102f53cdf327b4f2a4b99abc
aliff <aliff.rosli96@gmail.com>
2022-02-02 20:30:30 +0800

    compact variable in homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $workspaces = auth()->user()->workspaces;
+
+        return view('home', compact('workspaces'));
     }
 }
