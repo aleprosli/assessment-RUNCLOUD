5363460afb1418d404e8fd8b7abf41dd4327fce6
aliff <aliff.rosli96@gmail.com>
2022-02-02 20:58:00 +0800

    create policy showWorkspace for workspace

---
+++ app/Policies/WorkspacePolicy.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Workspace;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class WorkspacePolicy
+{
+    use HandlesAuthorization;
+
+    public function showWorkspace(User $user, Workspace $workspace)
+    {
+        return $user->id == $workspace->user_id;
+    }
+}
