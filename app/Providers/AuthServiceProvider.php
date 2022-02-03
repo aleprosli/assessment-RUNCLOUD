cc2a61a041c6951480d5a662c8936573c96237a3
aliff <aliff.rosli96@gmail.com>
2022-02-03 15:34:37 +0800

    register task policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Models\Workspace' => 'App\Policies\WorkspacePolicy',
+        'App\Models\Task' => 'App\Policies\TaskPolicy',
     ];
 
     /**
